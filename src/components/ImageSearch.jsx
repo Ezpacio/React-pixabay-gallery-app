cec6e4990c779a619e907622734e102aaeaceadd
Samet Gokkaya <sametgokkayax@gmail.com>
2024-02-12 15:03:20 +0300

    changed input text color

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -13,7 +13,7 @@ function ImageSearch({searchText}) {
          <form onSubmit={onSubmit} className="w-full max-w-sm">
             <div className="flex items-center border-b-2 border-teal-500 py-2">
                <input onChange={(e) => setText(e.target.value)} type="text" placeholder="Search Image Term..." className="appearance-none bg-transparent 
-               border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" />
+               border-none w-full text-gray-400 mr-3 py-1 px-2 leading-tight focus:outline-none focus:text-gray-600" />
                <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500
                hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="sumbit">
                   Search
